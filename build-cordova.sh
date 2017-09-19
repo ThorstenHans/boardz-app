fe74f0ca1b0213dd5a97451557d108675e97885a
Thorsten Hans <thorsten.hans@gmail.com>
2017-09-19 10:00:00 +0200

    feat(deps): updated cordova and electron build to download required packages only on demand

--- build-cordova.sh
+++ build-cordova.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-ECHO 'building the angular app for production...'
+echo 'building the angular app for production...'
 cd code
+npm install cordova --no-save
+npm install cordova-icon --no-save
+npm install cordova-splash --no-save
 npm run build --prod --base-ref="."
 cd ..
-ECHO 'generating temporary build dir...'
+echo 'generating temporary build dir...'
 rm -rf .tmp-cdv
 mkdir -p .tmp-cdv/www
 
-ECHO 'applying compiled angular app and cordova assets...'
+echo 'applying compiled angular app and cordova assets...'
 cp -rf dist/ .tmp-cdv/www
 cp ./code/cordova/config.xml ./code/cordova/package.json .tmp-cdv
 
@@ -16,13 +19,16 @@ cp -rf ./code/cordova/hooks ./.tmp-cdv
 
 
 cd .tmp-cdv
-ECHO 'restoring cordova dependencies for app...'
+echo 'restoring cordova dependencies for app...'
 ../code/node_modules/.bin/cordova prepare
-ECHO 'building app icon...'
+echo 'building app icon...'
 ../code/node_modules/.bin/cordova-icon --icon="../gfx/icon.png"
-ECHO 'building app spash screen...'
+echo 'building app spash screen...'
 ../code/node_modules/.bin/cordova-splash --splash="../gfx/splash.png"
 
-ECHO 'building cordova apps...'
+echo 'building cordova apps...'
 ../code/node_modules/.bin/cordova build
 cd ..
+
+echo ''
+echo 'to run the app execute "cd .tmp-cdv && ../code/node_modules/.bin/cordova emulate ios"'
--- build-electron.sh
+++ build-electron.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-ECHO 'building the angular app for production...'
+echo 'building the angular app for production...'
 cd code
+npm install electron --no-save
+npm install electron-packager --no-save
 npm run build --prod --base-ref="."
 cd ..
 
-ECHO 'compiling electron instruction file...'
+echo 'compiling electron instruction file...'
 ./code/node_modules/.bin/tsc ./code/electron/index.ts
 
-ECHO 'generating temporary build dir...'
+echo 'generating temporary build dir...'
 rm -rf .tmp-electron
 mkdir .tmp-electron
 cp code/electron/icon.png code/electron/index.js code/electron/package.json .tmp-electron
 cp -rf dist/ .tmp-electron
 
-ECHO 'building electron apps...'
+echo 'building electron apps...'
 rm -rf out
 ./code/node_modules/.bin/electron-packager .tmp-electron --electronVersion 1.7.6 --all --asar --icon ./gfx/icon --out out --overwrite
 
 
-ECHO 'cleaning up...'
+echo 'cleaning up...'
 rm -rf .tmp-electron
 open out
