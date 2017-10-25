f725b4e18c970de05c32455222912b4917aa8a5c
Thorsten Hans <thorsten.hans@gmail.com>
2017-10-25 05:34:00 +0200

    fix(build): updated electron build script to reflect recent folder renaming

--- build-electron.sh
+++ build-electron.sh
@@ -2,19 +2,19 @@
 
 echo 'installing electron-packager...'
 cd code
-npm install electron-packager
+npm install electron-packager --no-save
 
 echo 'building the angular app for production...'
 npm run build --prod --base-ref="."
 cd ..
 
 echo 'compiling electron instruction file...'
-./code/node_modules/.bin/tsc ./code/src-electron/index.ts
+./code/node_modules/.bin/tsc ./code-electron/index.ts
 
 echo 'generating temporary build dir...'
 rm -rf .tmp-electron
 mkdir .tmp-electron
-cp code/src-electron/icon.png code/src-electron/index.js code/src-electron/package.json .tmp-electron
+cp code-electron/icon.png code-electron/index.js code-electron/package.json .tmp-electron
 cp -rf dist/ .tmp-electron
 
 echo 'building electron apps...'
