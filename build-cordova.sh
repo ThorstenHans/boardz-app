5558658f19653f12765892912e33accd0d79c9bd
Thorsten Hans <thorsten.hans@gmail.com>
2017-10-25 05:17:15 +0200

    fix(build): updated cordova build script

--- build-cordova.sh
+++ build-cordova.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-echo 'building the angular app for production...'
+echo 'installing cordova tooling...'
 cd code
 npm install cordova cordova-icon cordova-splash
+
+echo 'building the angular app for production...'
 npm run build --prod --base-ref="."
 cd ..
+
 echo 'generating temporary build dir...'
 rm -rf .tmp-cdv
 mkdir -p .tmp-cdv/www
 
 echo 'applying compiled angular app and cordova assets...'
 cp -rf dist/ .tmp-cdv/www
-cp ./code/cordova/config.xml ./code/cordova/package.json .tmp-cdv
-
-cp -rf ./code/cordova/hooks ./.tmp-cdv
+cp ./code/src-cordova/config.xml ./code/src-cordova/package.json .tmp-cdv
+cp -rf ./code/src-cordova/hooks ./.tmp-cdv
 cd .tmp-cdv
 
 echo 'restoring cordova dependencies for app...'
 cordova prepare
+
 echo 'building app icon...'
 ./../code/node_modules/.bin/cordova-icon --icon="../gfx/icon.png"
+
 echo 'building app spash screen...'
 ./../code/node_modules/.bin/cordova-splash --splash="../gfx/splash.png"
 
