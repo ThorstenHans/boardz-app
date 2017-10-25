488ac3d74754a8f87f1d01274742d0c87a7189d8
Thorsten Hans <thorsten.hans@gmail.com>
2017-10-25 21:07:44 +0200

    fix(ts): don't use any as type for window

--- code/src/app/services/infrastructure/runtime.service.ts
+++ code/src/app/services/infrastructure/runtime.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 
-declare let window;
-
 @Injectable()
 export class RuntimeService {
 
@@ -32,15 +30,15 @@ export class RuntimeService {
     // IJS Electron demo hook
 
     public get isExcelApp(): boolean {
-        return this.isOfficeApp && Office.context.host == Office.HostType.Excel;
+        return this.isOfficeApp && Office.context.host === Office.HostType.Excel;
     }
 
     public get isOutlookApp(): boolean {
-        return this.isOfficeApp && Office.context.host == Office.HostType.Outlook;
+        return this.isOfficeApp && Office.context.host === Office.HostType.Outlook;
     }
 
     private guessPlatform(): void {
-        this._isMobile = !!window.cordova;
+        this._isMobile = window.hasOwnProperty('cordova');
         this._isDesktop = window.navigator.userAgent.match(/Electron/) !== null;
         this._isWeb = !(this._isMobile || this._isDesktop);
     }
